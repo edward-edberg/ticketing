e420dfe58c07db2385aa8415fb690133360c15e6
Edward Edberg <edward.edberg.halim@gmail.com>
2022-05-19 01:15:12 -0700

    done next js for authentication

--- client/pages/banana.js
+++
@@ -1,5 +0,0 @@
-const Banana = () => {
-  return <h1>Banana 5</h1>;
-};
-
-export default Banana;
--- client/pages/index.js
+++ client/pages/index.js
@@ -1,14 +1,13 @@
 import buildClient from "../api/build-client";
 
 const LandingPage = ({ currentUser }) => {
-  // console.log(currentUser);
-  // axios.get('/api/users/currentuser');
   console.log(currentUser);
 
   return <h1>{currentUser ? "You're signed in" : "You are not signed in"}</h1>;
 };
 
 LandingPage.getInitialProps = async (context) => {
+  console.log("LANDING PAGE");
   const client = buildClient(context);
   const { data } = await client.get("/api/users/currentuser");
   return data;
