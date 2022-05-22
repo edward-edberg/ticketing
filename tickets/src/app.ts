a85b5cae95b2bf3a307154bf39434aa5b19dcf48
Edward Edberg <edward.edberg.halim@gmail.com>
2022-05-21 23:33:37 -0700

    test index route, get all tickets

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -5,6 +5,7 @@ import mongoose from "mongoose";
 import cookieSession from "cookie-session";
 import { createTicketRouter } from "./routes/new";
 import { showTicketRouter } from "./routes/show";
+import { indexTicketRouter } from "./routes/index";
 
 import { errorHandler, NotFoundError, currentUser } from "@edod-tickets/common";
 
@@ -21,6 +22,7 @@ app.use(currentUser);
 
 app.use(createTicketRouter);
 app.use(showTicketRouter);
+app.use(indexTicketRouter);
 
 app.all("*", async (req, res, next) => {
   // next(throw new NotFoundError(););
