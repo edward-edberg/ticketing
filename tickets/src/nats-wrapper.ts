73131e707fc1062a9cc982c20b7a02dce084a5ec
Edward Edberg <edward.edberg.halim@gmail.com>
2022-05-24 14:39:19 -0700

    debug error

--- tickets/src/nats-wrapper.ts
+++ tickets/src/nats-wrapper.ts
@@ -3,12 +3,12 @@ import nats, { Stan } from "node-nats-streaming";
 class NatsWrapper {
   private _client?: Stan;
 
-  get client() {
-    if (!this._client) {
-      throw new Error("Cannot access NATS client before connecting");
-    }
-    return this._client;
-  }
+  // get client() {
+  //   if (!this._client) {
+  //     throw new Error("Cannot access NATS client before connecting");
+  //   }
+  //   return this._client;
+  // }
 
   connect(clusterId: string, clientId: string, url: string) {
     this._client = nats.connect(clusterId, clientId, { url });
@@ -19,7 +19,6 @@ class NatsWrapper {
         resolve();
       });
       this._client!.on("error", (err) => {
-        console.log("Error");
         reject(err);
       });
     });
